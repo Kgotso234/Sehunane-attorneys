46674f456b6cffe51823fa0ed7e671dcbf30816b
kgotsofatso <kgotsofatsompsane234@gmail.com>
2026-05-20 11:43:15 +0200

    fixed contact controller page

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,32 +1,45 @@
-public function send(Request $request)
-{
-    $data = $request->validate([
-        'name' => 'required|string|max:255',
-        'email' => 'required|email|max:255',
-        'phone' => 'required|string|max:20',
-        'subject' => 'required|string|max:255',
-        'message' => 'required|string',
-    ]);
+<? 
+    namespace App\Http\Controllers;
 
-    try {
+    use Illuminate\Support\Facades\Mail;
+    use Illuminate\Http\Request;
+    use App\Mail\ContactMessage;
+    use App\Mail\ContactAutoReply;
 
-        Mail::to('info@sehunaneattorneys.co.za')
-            ->send(new ContactMessage($data));
+    class ContactController extends Controller {
+        public function send(Request $request)
+        {
+            $data = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|email|max:255',
+                'phone' => 'required|string|max:20',
+                'subject' => 'required|string|max:255',
+                'message' => 'required|string',
+            ]);
 
-        Mail::to($data['email'])
-            ->send(new ContactAutoReply($data['name'], $data['subject']));
+            try {
 
-        return response()->json([
-            'success' => true,
-            'message' => 'Message sent successfully'
-        ]);
+                Mail::to('info@sehunaneattorneys.co.za')
+                    ->send(new ContactMessage($data));
 
-    } catch (\Exception $e) {
+                Mail::to($data['email'])
+                    ->send(new ContactAutoReply($data['name'], $data['subject']));
 
-        return response()->json([
-            'success' => false,
-            'message' => 'Mail failed',
-            'error' => $e->getMessage()
-        ], 500);
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Message sent successfully'
+                ]);
+
+            } catch (\Exception $e) {
+
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Mail failed',
+                    'error' => $e->getMessage()
+                ], 500);
+            }
+        }
     }
-}
+
+
+
