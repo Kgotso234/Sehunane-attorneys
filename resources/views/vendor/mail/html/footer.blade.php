208a1f8fc9b463f874b9cde927f803b3603deff2
kgotsofatso <kgotsofatsompsane234@gmail.com>
2026-05-22 23:30:17 +0200

    fixed emails template page

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,14 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+    <td class="footer" style="text-align: center; padding: 20px; font-size: 12px; color: #888;">
+        © {{ date('Y') }} Sehunane Attorneys Inc. All rights reserved.
+    </td>
+</tr>
+</td>
+</tr>
+</table>
+</td>
+</tr>
